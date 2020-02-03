0a86efceb8d6d4def531414d2b9b386b3f3c188d
Kevin Hattingh <kevin.hattingh@sintrex.com>
2020-02-03 09:27:53 +0200

    Add function makeSrcset to generate srcset values

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 // Get all image elements
 const IMAGES = document.querySelectorAll('img');
 
+// Function to construct responsive image paths
+function makeSrcset(imgSrc) {
+  let markup = [];
+  let width = 400;
+
+  for (let i = 0; i < 5; i++) {
+    markup[i] = `${imgSrc}-${width}.jpg ${width}w`;
+
+    width += 400;
+  }
+
+  return markup.join();
+}
+
 // Loop over image elements
 for (let i = 0; i < IMAGES.length; i++) {
   let imgSrc = IMAGES[i].getAttribute('src');
 
   imgSrc = imgSrc.slice(0, -8);
 
-  console.log(imgSrc);
+  let srcset = makeSrcset(imgSrc);
+
+  console.log(srcset);
 
   let type = IMAGES[i].getAttribute('data-type');
 
